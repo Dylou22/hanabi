1895ad7681e74b9902fcd6db67bf5834ec1347b5
KeyLawd <germain.vincen7@gmail.com>
2020-05-21 11:31:22 +0200

    Branche parallelle pour ne pas faire de betises pendant le markdown

---
+++ Le_bon_markdown_de_soutenance.md
@@ -0,0 +1,33 @@
+# Hanabi project
+
+Projet d'IN104 de 1ère année à l'ENSTA Paris
+Le but est de créer une IA capable de jouer au jeu de plateau Hanabi, à partir d'un module python fourni contenant une implémentation du jeu ainsi qu'une IA **"Cheater"** capable de voir son propre jeu.
+
+# Stratégie du chapeau
+
+Nous avons choisi d'implémenter une IA capable de jouer en suivant la stratégie dite *du chapeau*: lorsqu'un joueur doit donner un indicce, il calcule un nombre pour chaque autre joueur correspondant à sa recommandation de jeu (jeu ou défausse), le traduit en un indice donnable en jeu. Les autres joueurs sont capables de faire le calcul inverse et de déduire le nombre qui leur est attribué.
+
+## 1ère implémentation 
+
+Nous avons négligé quelques éléments de l'algorithme décrit dans l'article décrivant la stratégie dans cette version beta:
+-non prise en compte des cartes "indispensables" (i.e. celles que les joueurs ne doivent pas défausser sous peine de ne pas pouvoir atteindre un score parfait) : dans le cas où le joueur n'a pas de cartes "mortes" (dont le double a déjà été joué) dans la main lorsqu'il doit défausser, il défausse alors sa 1ère carte
+-nous avons décidé de fournir son nombre à chaque joueur, plutôt que d'écrire le calcul du décryptage juste après, ce qui nous semblait trop artificiel et peu intéressant.
+
+### Stratégie
+
+Afin de pouvoir décoder les indices, nous avons ajouté des attributs dans la classe **Card**:
+1. "recommanded" : vaut **False** par défaut. Lorsque le joueur décrypte un indice qui lui recommande de jouer la carte, l'attribut passe à **True**.
+2. "risky" : vaut **False** par défaut. Dès qu'un joueur joue une carte, que ce soit avec succès ou non, toutes les autres cartes déjà recommandées dans les mains des joueurs deviennent alors risquées : l'argument passe à **True**. Si la carte est recommandée à nouveau, l'attribut redevient **False**.
+3. "dead" : vaut **False** par défaut. Lorsque le joueur décrypte un indice qui lui recommande de défausser la carte, l'attribut passe à **True**. Si jamais on lui recommande de jouer la carte par la suite, l'attribut redevient **False**.
+
+On peut ainsi constituer une liste des cartes que le joueur devrait jouer (*recommanded == True and risky == False*), une liste que le joueur peut jouer si il y a peu de jetons rouges déjà en jeu (*recommanded == True and risky == True*) et une liste des cartes que le joueur devrait défausser (*dead == True*). Le calcul du nombre associé à la main du joueur est donc trivial.
+
+### Résulats
+
+Après un peu de bugfix (mauvaise implémentation de la liste des dead cards), l'IA ne perd déjà plus aucune partie. Les scores sont compris entre 10 et 25, avec un maximum à 17. (pour un échantillon de 10 000 parties)
+
+![Cheater IA vs Chapeau Beta](/images/logo.png)
+
+## 2è implémentation
+
+Après avoir tenté de 
---
+++ src/build/lib/hanabi/ai.py
@@ -0,0 +1,148 @@
+"""
+Artificial Intelligence to play Hanabi.
+"""
+
+import itertools
+
+
+class AI:
+    """
+    AI base class: some basic functions, game analysis.
+    """
+    def __init__(self, game):
+        self.game = game
+
+    @property
+    def other_hands(self):
+        "The list of other players' hands."
+        return self.game.hands[1:]
+
+    @property
+    def other_players_cards(self):
+        "All of other players's cards, concatenated in a single list."
+        # return sum([x.cards for x in self.other_hands], [])
+        return list(itertools.chain.from_iterable([hand.cards for hand in self.other_hands]))
+
+class Robot(AI):
+    def play(self):
+        game = self.game
+        jouables = [ i+1 for (i,card) in enumerate(game.current_hand.cards) if card.recommanded == True]
+        mortes = [ i+1 for (i,card) in enumerate(game.current_hand.cards) if card.dead == True]
+
+#choix de l'action a effectuer en fonction de la main du joueur (playable card, most recent recomandation, no card played, hint tokken, discardable card)
+#			=>playable card : laquelle ?
+#			=>hint : à qui et quoi ?
+#			=>dicardable card : indice le plus faible
+#maj du tableau de jeu
+        if (jouables!=[]):
+            if (game.current_hand.cards[jouables[0]-1].risky == False):
+                return("p%d"%jouables[0])
+            elif(game.current_hand.cards[jouables[0]-1].risky == True and game.red_coins < 2):
+                return("p%d"%jouables[0])
+#action 3 : si on peut donner un indice on le donne
+        if (game.blue_coins>0):
+            n_propre = game.somme_joueurs
+            if n_propre == 0:
+                return("c11")
+            if n_propre == 1:
+                return("c12")
+            if n_propre == 2:
+                return("c13")
+            if n_propre == 3:
+                return("c14")
+            if n_propre == 4:
+                return("cR1")
+            if n_propre == 5:
+                return("cR2")
+            if n_propre == 6:
+                return("cR3")
+            if n_propre == 7:
+                return("cR4")
+#action 4 : si la dernière recommandation était de jeter une carte, jetée la carte
+        if mortes != []:
+            return("d%d"%mortes[0])
+#action 5 : carte c1 jetée
+        else:
+            return("d1")
+
+
+class Cheater(AI):
+    """
+    This player can see his own cards!
+    Algorithm:
+      * if 1-or-more card is playable: play the lowest one, then newest one
+      * if blue_coin<8 and an unnecessary card present: discard it.
+      * if blue_coin>0: give a clue on precious card (so a human can play with a Cheater)
+      * if blue_coin<8: discard the largest one, except if it's the last of its kind or in chop position in his opponent.
+    """
+
+    def play(self):
+        "Return the best cheater action."
+        game = self.game
+        playable = [ (i+1, card.number) for (i, card) in
+                     enumerate(game.current_hand.cards)
+                     if game.piles[card.color]+1 == card.number ]
+
+        if playable:
+            # sort by ascending number, then newest
+            playable.sort(key=lambda p: (p[1], -p[0]))
+            return "p%d"%playable[0][0]
+
+        #
+        discardable = [ i+1 for (i, card) in
+                        enumerate(game.current_hand.cards)
+                        if ( (card.number <= game.piles[card.color])
+                             or (game.current_hand.cards.count(card) > 1)
+                        ) ]
+        # discard already played cards, doubles in my hand
+        # fixme: discard doubles, if I see it in partner's hand
+        # fixme: il me manque les cartes sup d'une pile morte
+
+        if discardable and (game.blue_coins < 8):
+            return "d%d"%discardable[0]
+
+        ## 2nd type of discard: I have a card, and my partner too
+
+        discardable2 = [ i+1 for (i, card) in enumerate(game.current_hand.cards)
+                         if card in self.other_players_cards
+                       ]
+        if discardable2 and (game.blue_coins < 8):
+            return "d%d"%discardable2[0]
+
+
+        ## Look at precious cards in other hand, to clue them
+        precious = [ card for card in
+                     self.other_players_cards
+                     if (1+game.discard_pile.cards.count(card))
+                         == game.deck.card_count[card.number]
+                   ]
+        if precious:
+            clue = False
+            for p in precious:
+                if p.number_clue is False:
+                    clue = "c%d"%p.number
+                    break
+                if p.color_clue is False:
+                    clue = "c%s"%p.color
+                    clue = clue[:2]
+                    break
+            if clue:
+                if game.blue_coins > 0:
+                    return clue
+        if game.blue_coins > 0:
+            return 'cw'
+        mynotprecious = [ (card.number, i+1) for (i, card) in
+                          enumerate(game.current_hand.cards)
+                          if not (
+                                  (1+game.discard_pile.cards.count(card))
+                                  == game.deck.card_count[card.number])
+                     ]
+        mynotprecious.sort(key=lambda p: (-p[0], p[1]))
+        if mynotprecious:
+            act = 'd%d'%mynotprecious[0][1]
+            return act
+        myprecious = [ (card.number, i+1) for (i, card) in enumerate(game.current_hand.cards) ]
+        myprecious.sort(key=lambda p: (-p[0], p[1]))
+        act = 'd%d'%myprecious[0][1]
+        return act
+
